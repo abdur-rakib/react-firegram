a3f72936aef14ca6f6ec5a4166afc9776f4d2f71
abdur-rakib <abdurrakib961@gmail.com>
2020-07-30 11:13:44 +0300

    added firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyDo3KxC0y-EcRW2qlNgivVHTEmsQ4JQ8B4",
+  authDomain: "react-firegram-d2218.firebaseapp.com",
+  databaseURL: "https://react-firegram-d2218.firebaseio.com",
+  projectId: "react-firegram-d2218",
+  storageBucket: "react-firegram-d2218.appspot.com",
+  messagingSenderId: "86750579510",
+  appId: "1:86750579510:web:6ce74dd48d04ffa8050197",
+};
+
+export default config;
